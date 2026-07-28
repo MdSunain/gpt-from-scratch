f208e5d43e2e5d5699644cc981a0845d99bf135a
MdSunain <mdsunain218@gmail.com>
2026-07-28 15:05:15 +0530

    SunainGPT-v2

---
+++ bigram.py
@@ -0,0 +1,109 @@
+import torch 
+import torch.nn as nn
+from torch.nn import functional as F
+
+batch_size = 32
+block_size = 8
+max_iterations = 3000
+eval_intervals = 300
+learning_rate = 1e-3
+eval_iterations = 200
+
+torch.manual_seed(1337)
+
+with open('input.txt', 'r', encoding='utf-8') as f:
+    data = f.read()
+
+chars = sorted(list(set(data)))
+vocab_size = len(chars)
+
+s_to_i = {ch:i for i,ch in enumerate(chars)} # encrypt mapping
+i_to_s = {i:ch for i,ch in enumerate(chars)} # decrypt mapping
+
+def encode(s):
+    return [s_to_i[c] for c in s]
+def decode(i):
+    return ''.join([i_to_s[c] for c in i])
+
+encrypted_data = torch.tensor(encode(data), dtype= torch.long)
+
+n = int(0.9*len(encrypted_data))
+
+train_data = encrypted_data[:n]
+test_data = encrypted_data[n:]
+
+# data loading
+def get_batch(split):
+    data = train_data if split == 'train' else test_data
+
+    ix = torch.randint(len(encrypted_data)-block_size, (batch_size,))
+    x = torch.stack([encrypted_data[i:i+block_size] for i in ix] )
+    y = torch.stack([encrypted_data[i+1:i+block_size+1] for i in ix])
+    return x,y
+
+def estimated_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iterations)
+        for k in range(eval_iterations):
+            X,Y = get_batch(split)
+            logits, loss = model(X,Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+# super simple bigram model
+class bigramLanguageModel(nn.Module):
+    def __init__(self,vocab_size):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size,vocab_size)
+
+    def forward(self, idx, targets = None):
+        logits = self.token_embedding_table(idx) # B T C
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):# idx is (B,T) array of indices
+        for _ in range(max_new_tokens):
+            # get the prediction
+            logits, loss = self(idx)
+            # focus only on the last step
+            logits = logits[:,-1,:]# (B,C)
+            # apply softmax for probabilities
+            prob = F.softmax(logits, dim= -1)
+            # sample from the distribution
+            idx_next = torch.multinomial(prob, num_samples=1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
+
+    
+
+model = bigramLanguageModel(vocab_size)
+
+# create a pytorch optimizer 
+optimizer= torch.optim.Adam(model.parameters(), lr=1e-3)
+
+for iters in range(max_iterations):
+    if iters % eval_intervals == 0:
+        losses = estimated_loss()
+        print(f"step {iters}: train loss{losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+    xb, yb = get_batch('train')
+
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+context = torch.zeros((1,1) ,dtype=torch.long)
+print(decode(model.generate(context, max_new_tokens=500)[0].tolist()))
